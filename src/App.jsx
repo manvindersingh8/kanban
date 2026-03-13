40b8cfb0ec340b03a36940599e7ec4c26c8616e2
manvinder <manvindersinghone@gmail.com>
2026-03-13 20:30:37 +0530

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -18,12 +18,14 @@ const App = () => {
     setTasks((prev) => [...prev, task]);
     setNewTask("");
   };
+
   const deleteTask = (id) => {
     const updatedTasks = tasks.filter((ele) => {
       return ele.id !== id;
     });
     setTasks(updatedTasks);
   };
+
   useEffect(() => {
     console.log(tasks);
   }, [tasks]);
@@ -38,75 +40,92 @@ const App = () => {
 
   const handleDrop = (status) => {
     if (!draggedTask) return;
+
     const updatedTasks = tasks.map((ele) => {
       return ele.id === draggedTask.id ? { ...ele, status } : ele;
     });
+
     setTasks(updatedTasks);
     setDraggedTask(null);
   };
 
   return (
     <>
-      <div className="h-screen bg-gradient-to-br from-zinc-700 to-zinc-500 flex justify-center items-center">
-        <div className="bg-white">
-          <div className="bg-gradient-to-t bg-gray-300-500 p-5 flex gap-x-28 ">
+      <div className="h-screen bg-gradient-to-r from-zinc-800 to-zinc-500 flex justify-center items-center ">
+        <div className="bg-gradient-to-r from-blue-200 via-violet-300 to-blue-300 rounded-lg max-w-screen-lg]:">
+          <div className="p-6 flex gap-x-32">
             <input
               type="text"
               placeholder="Add new task"
               onChange={(e) => setNewTask(e.target.value)}
               value={newTask}
-              className="bg-gray-500 p-3"
+              className="bg-white border-2 border-blue-500 rounded-2xl placeholder:text-center text-center w-96"
             />
+
             <select
+              value={taskStatus}
               onChange={(e) => setTaskStatus(e.target.value)}
-              className="bg-gray-500 p-3 text-white"
+              className={`rounded-2xl text-center p-2 ${
+                taskStatus==='todo'
+                ?'bg-indigo-400'
+                :taskStatus==='progress'
+                ?'bg-yellow-300':
+                'bg-green-400'
+              }`}
             >
-              <option value="todo">todo</option>
-              <option value="progress">progress</option>
-              <option value="done">done</option>
+              <option  className="bg-white"  value="todo">Todo</option>
+              <option className="bg-white"   value="progress">Progress</option>
+              <option  className="bg-white" value="done">Done</option>
             </select>
-            <button className="bg-gray-500 p-3" onClick={addTask}>
-              AddTask
+
+            <button
+              className="bg-blue-600 hover:bg-blue-700 text-white p-3 rounded-lg transition"
+              onClick={addTask}
+            >
+              Add Task
             </button>
           </div>
 
-          <div className="flex justify-evenly items-start">
+          <div className="flex justify-evenly items-start p-6">
             <div>
-              <h1 className="text-xl font-bold text-gray-800 border-b-2 border-gray-300 pb-2 mb-3">
+              <h1 className="  w-60 text-xl text-center font-bold bg-indigo-400 border-b-2 border-gray-300 pb-2 mb-3">
                 Todo
               </h1>
-              <ul onDragOver={handleDragOver} onDrop={() => handleDrop("todo")}>
+
+              <ul 
+              
+              onDragOver={handleDragOver} 
+              onDrop={() => handleDrop("todo")}
+              className="bg-indigo-400 rounded-3xl text-center"
+              >
                 {tasks
                   .filter((e) => e.status === "todo")
                   .map((ele) => {
                     return (
-                      <>
-                        <li
-                          key={ele.id}
-                          draggable
-                          onDragStart={() => handleDragStart(ele)}
-                        >
-                          {ele.name}{" "}
-                          <button
-                            onClick={() => {
-                              deleteTask(ele.id);
-                            }}
-                          >
-                            delete
-                          </button>
-                        </li>
-                      </>
+                      <li
+                        key={ele.id}
+                        draggable
+                        onDragStart={() => handleDragStart(ele)}
+                      >
+                        {ele.name}{" "}
+                        <button
+                        className="bg-red-500 hover:bg-red-700 text-white p-1 rounded-lg transition"
+                        onClick={() => deleteTask(ele.id)}>
+                          delete
+                        </button>
+                      </li>
                     );
                   })}
               </ul>
             </div>
 
             <div>
-              <h1 className="text-xl font-bold text-gray-800 border-b-2 border-gray-300 pb-2 mb-3">
+              <h1 className="  w-60 text-xl text-center font-bold text-gray-800 bg-yellow-200 border-b-2 border-gray-300 pb-2 mb-3">
                 progress
               </h1>
 
               <ul
+               className="bg-yellow-300 rounded-3xl text-center"
                 onDragOver={handleDragOver}
                 onDrop={() => handleDrop("progress")}
               >
@@ -114,51 +133,46 @@ const App = () => {
                   .filter((e) => e.status === "progress")
                   .map((ele) => {
                     return (
-                      <>
-                        <li
-                          key={ele.id}
-                          draggable
-                          onDragStart={() => handleDragStart(ele)}
-                        >
-                          {ele.name}{" "}
-                          <button
-                            onClick={() => {
-                              deleteTask(ele.id);
-                            }}
-                          >
-                            delete
-                          </button>
-                        </li>
-                      </>
+                      <li
+                        key={ele.id}
+                        draggable
+                        onDragStart={() => handleDragStart(ele)}
+                      >
+                        {ele.name}{" "}
+                        <button
+                         className="bg-red-500 hover:bg-red-700 text-white p-1 rounded-lg transition"
+                        
+                        onClick={() => deleteTask(ele.id)}>
+                          delete
+                        </button>
+                      </li>
                     );
                   })}
               </ul>
             </div>
+
             <div>
-              <h1 className="text-xl font-bold text-gray-800 border-b-2 border-gray-300 pb-2 mb-3">
+              <h1 className=" w-60 text-center px-5 text-xl font-bold text-gray-800 bg-green-500 border-b-2 border-gray-300 pb-2 mb-3">
                 done
               </h1>
-              <ul onDragOver={handleDragOver} onDrop={() => handleDrop("done")}>
+
+              <ul onDragOver={handleDragOver} onDrop={() => handleDrop("done")} className="bg-green-500 rounded-3xl text-center">
                 {tasks
                   .filter((e) => e.status === "done")
                   .map((ele) => {
                     return (
-                      <>
-                        <li
-                          key={ele.id}
-                          draggable
-                          onDragStart={() => handleDragStart(ele)}
-                        >
-                          {ele.name}{" "}
-                          <button
-                            onClick={() => {
-                              deleteTask(ele.id);
-                            }}
-                          >
-                            delete
-                          </button>
-                        </li>
-                      </>
+                      <li
+                        key={ele.id}
+                        draggable
+                        onDragStart={() => handleDragStart(ele)}
+                      >
+                        {ele.name}{" "}
+                        <button
+                        className="bg-red-500 hover:bg-red-700 text-white p-1 rounded-lg transition"
+                         onClick={() => deleteTask(ele.id)}>
+                          delete
+                        </button>
+                      </li>
                     );
                   })}
               </ul>
